5f9e1da5bf359d730cec7fd2a4bcd4f978f5b3df
byaxe <byaxe.sx@gmail.com>
2016-04-28 17:20:49 +0300

    Day2

--- js.js
+++ js.js
@@ -2,18 +2,23 @@
  * Created by byaxe on 4/27/16.
  */
 $(function () {
-    arrayOfNames = [['fadeInRight', 3000], ['flash', 2000], ['hinge', 1000]];
-    foo('.foo1', arrayOfNames, 0, 0);
+    // arrayOfNames = [['fadeInRight', 3000], ['flash', 2000], ['hinge', 1000]];
+    // foo('.foo1', arrayOfNames, 0, 0);
+    $('.foo2').on('click',function () {
+        $(this).toggleClass('bounce animated');
+    });
 });
 
 
 function foo(selectorName, classNameArray, step, duration) {
 
-    duration = 0 == step ? 0 : classNameArray[step - 1][1];
+    duration = (0 == step) ? 0 : (classNameArray[step - 1][1]);
     setTimeout(function () {
         console.log('animation begin. classNameArray[step][0]= ' + classNameArray[step][1] + " ");
-        step-1==0? $(selectorName).removeClass(classNameArray[step-1][0]):"";
-        $(selectorName).attr('style', 'animation-duration:' + (parseInt(classNameArray[step][1]/1000)) + "s;");
+
+        step - 1 == 0 ? $(selectorName).removeClass(classNameArray[step - 1][0]) : "";
+
+        $(selectorName).attr('style', 'animation-duration:' + (parseInt(classNameArray[step][1] / 1000)) + "s;");
         $(selectorName).addClass(String(classNameArray[step][0]));
 
         step = step + 1;
